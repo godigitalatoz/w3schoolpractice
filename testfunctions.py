c06642dd39f88bff37c5ebadb475169b3a12ffa4
godigitalatoz <134773995+godigitalatoz@users.noreply.github.com>
2023-07-16 14:25:12 +0500

    Update testfunctions.py

--- testfunctions.py
+++ testfunctions.py
@@ -1,1 +1,24 @@
-help(for)
+import turtle
+
+# Create a turtle object
+my_turtle = turtle.Turtle()
+
+# Set the speed of the turtle
+my_turtle.speed(5)
+
+# Draw a square
+i = 100
+while i > 0:
+    i -=10
+    side_length = i
+
+for _ in range(8):
+    my_turtle.forward(side_length)
+    my_turtle.left(45)
+
+# Hide the turtle
+my_turtle.hideturtle()
+
+# Keep the window open
+turtle.done()
+
